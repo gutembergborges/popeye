d2edcf877325aa015e54044e99e48224f294fd26
Gutemberg Borges <gutem.borges@gmail.com>
2019-10-30 11:51:53 -0300

    created website blade views and routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,28 @@
 |
 */
 
+// DEPOIS COLOCAR PARA OS CONTROLLERS REDIRECIONAR
+/** Website */
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('sobre', function () {
+    return view('about');
+});
+Route::get('contato', function () {
+    return view('contact');
+});
+Route::get('galeria', function () {
+    return view('gallery');
+});
+Route::get('pacotes', function () {
+    return view('packages');
+});
+Route::get('precos', function () {
+    return view('pricing');
+});
+
+/** Admin Panel */
 Route::get('admin', function () {
     return view('admin');
 });
