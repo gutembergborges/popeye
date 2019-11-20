97f1b0fe7a03fab475e9a0b1f7b39e8625f0ceb4
Gutemberg Borges <gutem.borges@gmail.com>
2019-11-20 18:07:09 -0300

    created pricing plan section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -64,45 +64,7 @@
 
       @include('sections.our-packages')
 
-      <div class="site-section">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-4">
-              <span class="text-cursive h5 text-red d-block">Pricing Plan</span>
-              <h2 class="text-black">Our Pricing</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo quis cupiditate fugit, voluptatibus ullam, non laboriosam alias veniam, ex inventore iure sed?</p>
-            </div>
-            <div class="col-md-4">
-              <div class="pricing teal">
-                <span class="price">
-                  <span>$30</span>
-                </span>
-                <h3>Silver Pack</h3>
-                <ul class="list-unstyled">
-                  <li><i class="fas fa-check text-teal"></i> Lorem ipsum dolor sit amet</li>
-                  <li><i class="fas fa-check text-teal"></i> Consectetur adipisicing elit</li>
-                  <li><i class="fas fa-check text-teal"></i> Nemo quis cupiditate</li>
-                </ul>
-                <p><a href="#" class="btn btn-teal btn-custom-1 mt-4">Buy Now</a></p>
-              </div>
-            </div>
-            <div class="col-md-4">
-              <div class="pricing danger">
-                <span class="price">
-                  <span>$70</span>
-                </span>
-                <h3>Golden Pack</h3>
-                <ul class="list-unstyled danger">
-                  <li><i class="fas fa-check text-danger"></i> Lorem ipsum dolor sit amet</li>
-                  <li><i class="fas fa-check text-danger"></i> Consectetur adipisicing elit</li>
-                  <li><i class="fas fa-check text-danger"></i> Nemo quis cupiditate</li>
-                </ul>
-                <p><a href="#" class="btn btn-danger btn-custom-1 mt-4">Buy Now</a></p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      @include('sections.pricing-plan')
 
       <div class="site-section bg-light">
         <div class="container">
--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -23,45 +23,7 @@
     </div>
 
 
-    <div class="site-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-4">
-            <span class="text-cursive h5 text-red d-block">Pricing Plan</span>
-            <h2 class="text-black">Our Pricing</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo quis cupiditate fugit, voluptatibus ullam, non laboriosam alias veniam, ex inventore iure sed?</p>
-          </div>
-          <div class="col-md-4">
-            <div class="pricing teal">
-              <span class="price">
-                <span>$30</span>
-              </span>
-              <h3>Silver Pack</h3>
-              <ul class="ul-check list-unstyled teal">
-                <li>Lorem ipsum dolor sit amet</li>
-                <li>Consectetur adipisicing elit</li>
-                <li>Nemo quis cupiditate</li>
-              </ul>
-              <p><a href="#" class="btn btn-teal btn-custom-1 mt-4">Buy Now</a></p>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="pricing danger">
-              <span class="price">
-                <span>$70</span>
-              </span>
-              <h3>Golden Pack</h3>
-              <ul class="ul-check list-unstyled danger">
-                <li>Lorem ipsum dolor sit amet</li>
-                <li>Consectetur adipisicing elit</li>
-                <li>Nemo quis cupiditate</li>
-              </ul>
-              <p><a href="#" class="btn btn-danger btn-custom-1 mt-4">Buy Now</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    @include('sections.pricing-plan')
 
     <div class="site-section bg-light">
       <div class="container">
