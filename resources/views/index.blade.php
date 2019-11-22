4f71694af3b401224217aa728c2cf81dae807cf2
Gutemberg Borges <gutem.borges@gmail.com>
2019-11-22 11:14:13 -0300

    created banner fragment section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -68,17 +68,7 @@
 
       @include('sections.testimonial')
 
-
-      <div class="site-section py-5 bg-warning">
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-md-12 d-flex">
-            <h2 class="text-white m-0">Bring Fun Life To Your Kids</h2>
-            <a href="#" class="btn btn-primary btn-custom-1 py-3 px-5 ml-auto">Get Started</a>
-            </div>
-          </div>
-        </div>
-      </div>
+      @include('sections.banner')
 
       @include('sections.footer')
 
@@ -98,7 +88,8 @@
     <script src="js/bootstrap-datepicker.min.js"></script>
     <script src="js/aos.js"></script>
 
-    <script src="js/main.js"></script> --}}
+    <script src="js/main.js"></script>
+--}}
 
   </body>
 
--- resources/views/packages.blade.php
+++ resources/views/packages.blade.php
@@ -28,17 +28,7 @@
 
     @include('sections.testimonial')
 
-
-    <div class="site-section py-5 bg-warning">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-md-12 d-flex">
-          <h2 class="text-white m-0">Bring Fun Life To Your Kids</h2>
-          <a href="#" class="btn btn-primary btn-custom-1 py-3 px-5 ml-auto">Get Started</a>
-          </div>
-        </div>
-      </div>
-    </div>
+    @include('sections.banner')
 
     @include('sections.footer')
 
